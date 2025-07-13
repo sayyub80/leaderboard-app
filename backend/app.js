5684d610a2bc28c2a7477b4761dec9ec8be6b537
sayyub80 <mdsayyub80@gmail.com>
2025-07-13 21:42:39 +0530

    cors fixed

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "https://leader-task.netlify.app", //frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    credentials: true, // Allow cookies (if needed)
+  })
+);
 app.use(express.json());
 
 // Routes
